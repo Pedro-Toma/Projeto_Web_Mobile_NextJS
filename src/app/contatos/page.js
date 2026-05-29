6900516ff6b21a523bf595d7263d6545efb9e0f8
PedroToma <113950175+Pedro-Toma@users.noreply.github.com>
2026-05-29 01:28:33 +0000

    Correção de bug na paágina de contatos

--- src/app/contatos/page.js
+++ src/app/contatos/page.js
@@ -1,23 +1,23 @@
 import styles from './page.module.css';
 
-export default function sobreNos(){
+export default function contatos(){
     return (
         <>
-        <h2 className={styles.titulo}>
-            Fale Conosco
-        </h2>
+            <h2 className={styles.titulo}>
+                Fale Conosco
+            </h2>
 
-        <p className={styles.texto}>
-            Caso precise de alguma ajuda, mande mensagem através dos contatos abaixo:
-        </p>
+            <p className={styles.texto}>
+                Caso precise de alguma ajuda, mande mensagem através dos contatos abaixo:
+            </p>
 
-        <p className={styles.texto}>
-            Email: contato@listaBarata.com.br
-        </p>
+            <p className={styles.texto}>
+                Email: contato@listaBarata.com.br
+            </p>
 
-        <p className={styles.texto}>
-            Telefone: (11)92568-6536
-        </p>
-        </main>
+            <p className={styles.texto}>
+                Telefone: (11)92568-6536
+            </p>
+        </>
     );
 }
